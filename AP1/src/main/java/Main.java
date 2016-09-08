c5ef91782e992b4488f33044ffbddea2c39230bd
Jaronimous <rienja@live.nl>
2016-09-08 14:25:26 +0200

    test

--- AP1/src/main/java/Main.java
+++ AP1/src/main/java/Main.java
@@ -14,6 +14,7 @@ public class Main implements CalculatorInterface {
 
     public Double rpn(TokenList tokens) {
         // TODO: Implement this
+    	//go sucka dick
         return null;
     }
 
